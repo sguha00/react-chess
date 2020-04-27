2104b0d740b006c593206719680420af386b3e89
Shayan Guha <shayanguha@gmail.com>
2020-04-27 16:49:07 -0700

    Fix styling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import Game from './components/game.js'
+import Game from './components/game'
 
 
 ReactDOM.render(
